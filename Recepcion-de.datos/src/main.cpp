7b791016a2a43cfb98497422d13a1d1679df80e9
andres922121 <54625899+andres922121@users.noreply.github.com>
2019-08-30 06:07:06 -0500

    comunicador por hiperterminal
    comunicacion por hiperterminal con condicion

---
+++ Recepcion-de.datos/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);
+
+}
+
+void loop() {
+  if (Serial.available()>0)
+  {
+   char Dato = Serial.read();
+     if (Dato == 'A') digitalWrite(2,HIGH);
+       else if (Dato == 'B')digitalWrite(2,LOW);
+      
+      
+     
+     
+
+
+
+
+  }
+  
+
+
+
+
+}
